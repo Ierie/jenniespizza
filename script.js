f7327805ac65ccc61e0c9c50b76b8c98cfdf233c
Ierie Marie Manguit <empressre14@gmail.com>
2023-04-08 23:12:52 +0800

    created menu categories Home.html

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ $(document).ready(function () {
       $('.animated-icon3').toggleClass('open');
     });
   });
+
+  function openCategory(listm, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    listm.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
